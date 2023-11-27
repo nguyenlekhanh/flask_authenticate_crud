4df6ad46734f71cb0f6fb9f1c498377e0ab7fdeb
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-11-26 23:23:28 -0600

    create database

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -8,7 +9,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'ksd fkf ksj fskj '
-    app.config['SQLALCHEMY_DATABASE_URL'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
     db.init_app(app)
 
@@ -18,4 +19,15 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+    
+    with app.app_context():
+        db.create_all()
+        print('Created Database!')
+
     return app
+
+# def create_database(app):
+#     if not path.exists('website/' + DB_NAME):
+#         db.create_all(app=app)
+#         print('Created Database!')
